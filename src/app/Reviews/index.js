2d862e1b82b1ded3d2e59522f52dec07113afb0d
femzy123 <femzy123@yahoo.com>
2023-08-13 13:18:58 +0100

    Add background svg

--- src/app/Reviews/index.js
+++ src/app/Reviews/index.js
@@ -4,7 +4,7 @@ import ReviewCard from './ReviewCard';
 
 const Reviews = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3'>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-[30px]'>
       {reviewData.map((review, i) => (
         <ReviewCard key={i} rating={review.rating} comment={review.comment} author={review.author} />
       ))}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Reviews from './Reviews';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Reviews from "./Reviews";
 
 export default function ReviewPage() {
-  return <div className="text-3xl font-bold">
-    <Reviews />
-  </div>;
+  return (
+    <div class="bg-[#FFF1E8] relative flex items-center justify-center min-h-screen px-8 py-[112px] lg:px-[135px]">
+      <div
+        class="fixed top-0 right-0 w-[415px] h-[415px] z-0 bg-no-repeat bg-center bg-cover transform translate-x-1/2"
+        style={{ backgroundImage: "url('/graphics/Accent.svg')" }}
+      ></div>
+      <div
+        class="fixed bottom-0 left-0 w-[415px] h-[415px] z-0 bg-no-repeat bg-center bg-cover transform -translate-x-1/2"
+        style={{ backgroundImage: "url('/graphics/Accent.svg')" }}
+      ></div>
+      <div className="relative z-10">
+        <Reviews />
+      </div>
+    </div>
+  );
 }
